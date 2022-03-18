7eae9b2b500c9360e0dd5bf82908cd6a21684bf6
Matthew <83066475+MatFatCat@users.noreply.github.com>
2022-03-18 21:18:47 +0300

    init project

---
+++ ChessByMatthew/ChessByMatthewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessByMatthewApp.swift
+//  ChessByMatthew
+//
+//  Created by Matthew Popov on 18.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChessByMatthewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChessByMatthew/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChessByMatthew
+//
+//  Created by Matthew Popov on 18.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
